e826a66f072f7c6090939b350adf6e8718aca01d
SamarpreetSingh787 <78362149+SamarpreetSingh787@users.noreply.github.com>
2021-03-31 04:39:23 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-//https://teachablemachine.withgoogle.com/models/3jyzdCyWa/
+prediction = "";
+
+Webcam.set( {
+  width: 350 ,
+  height: 300 , 
+  image_format: "png" ,
+  png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(params) {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_img" src="'+data_uri+'">';
+    })
+}
+
+console.log("ml5 version: " , ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3jyzdCyWa/model.json" , modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data1 = "The first prediction is " + prediction_1;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1);
+    synth.speak(utterThis);
+}
